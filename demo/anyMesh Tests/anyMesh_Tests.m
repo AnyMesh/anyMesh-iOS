c4b604f2529ebf541237757fdaada2d5cc59911c
Dave Paul <davepaul0@gmail.com>
2014-07-03 06:34:30 -0500

    Added unit tests back into project

---
+++ demo/anyMesh Tests/anyMesh_Tests.m
@@ -0,0 +1,34 @@
+//
+//  anyMesh_Tests.m
+//  anyMesh Tests
+//
+//  Created by David Paul on 7/3/14.
+//  Copyright (c) 2014 dpTools. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface anyMesh_Tests : XCTestCase
+
+@end
+
+@implementation anyMesh_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTAssertEqual(1, 1, @"test");
+}
+
+@end
